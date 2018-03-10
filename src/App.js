9920252f6aebae8b273a230b5354283e956a4a91
Pavtru <trupinapavle2012@gmail.com>
2018-03-10 16:28:44 +0100

    Onclick handleclick function test

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import './App.css';
 class App extends Component {
 constructor(){
   super();
+
    this.state = {gethdr: "Welcome to React"};
 }
 handleclick(gethdr){
-
+  this.setState({color: "blue"});
 }
   render() {
      
@@ -18,7 +19,7 @@ handleclick(gethdr){
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" onClick={this.handleclick.bind(this)}>{this.state.gethdr}</h1>
+          <h1 className="App-title" style={color:"black"} onClick={this.handleclick.bind(this)}>{this.state.gethdr}</h1>
           
         </header>
         <input />
