ade54b78380310e9e21bb927dc5e977c6270f4fc
Pavtru <trupinapavle2012@gmail.com>
2018-03-10 14:23:57 +0100

    Changed header variable to get from new method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  gethdr(){
+    return "Welcome to React"
+  }
   render() {
-    const hdr = "Welcome to React";
+     
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" >{hdr}</h1>
+          <h1 className="App-title"  >{this.gethdr()}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
