1a52771fd017a86d1ef2315c11484c3bedb7528e
Pavtru <trupinapavle2012@gmail.com>
2018-03-10 16:20:51 +0100

    Instead of method added constructor and function to handle onclick event

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  gethdr(){
-    const hdr ="Welcome to React";
-    
-    return hdr  }
+constructor(){
+  super();
+   this.state = {gethdr: "Welcome to React"};
+}
+handleclick(gethdr){
+
+}
   render() {
      
     return (
+      
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" onclick={this.gethdr} >{this.gethdr()}</h1>
+          <h1 className="App-title" onClick={this.handleclick.bind(this)}>{this.state.gethdr}</h1>
+          
         </header>
+        <input />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
