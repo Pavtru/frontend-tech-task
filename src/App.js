8d679911dffa19a6c57ec210d2b8d375e1ec61bf
Pavle Trupina <trupinapavle2012@gmail.com>
2018-03-11 20:26:38 +0100

    Separated fetch func. from onclick handle function and added "#" infront of hash numbers (still with issues)

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ class App extends Component {
 constructor(){
   super();
    this.state = {gethdr: "Welcome to React"};
+   
 }
 
-
+fetchfn(col2,col3){
+  fetch("http://www.colr.org/json/color/random")
+  .then((resp)=>{ return resp.json() })
+  .then((json)=>{col3 = col2 + json.colors[0].hex;  console.log(col3);})
+  .then((finval)=> {this.setState(finval);})
+  
+ return col3
+}
 
 handleclick(){
   
   var col1 = "black";
-  var col2 = "blue";
-  
-  fetch("http://www.colr.org/json/color/random")
-  .then((resp)=>{ return resp.json() })
-  .then((json)=>{ console.log(json.colors[0].hex)});
-  
+  var col2 = "#";
+  var col3 ="";
+  this.fetchfn(col2,col3)
 
-
-
-
-
-
- if (this.state.color !== "black"){
+  if (this.state.color !== "black"){
    this.setState({color: col1});
  }
  else
